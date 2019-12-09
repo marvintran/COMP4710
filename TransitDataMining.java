74c02c09572abf406418666b1a7c0cf01340adf8
marvintran <tranm346@myumanitoba.ca>
2019-12-09 00:29:39 -0600

    Implemented DecisionTree and provided temporary sample data

--- TransitDataMining.java
+++ TransitDataMining.java
@@ -1,8 +1,14 @@
-import smile.association.FPGrowth;
-import smile.association.FPTree;
-import smile.association.ItemSet;
+import org.apache.commons.csv.CSVFormat;
+import smile.association.*;
+import smile.classification.DecisionTree;
+import smile.data.DataFrame;
+import smile.data.formula.Formula;
+import smile.data.type.StructType;
+import smile.io.Arff;
+import smile.io.CSV;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,12 +16,13 @@ import java.util.stream.Stream;
 public class TransitDataMining {
 
   public static void main(String[] args) {
-    String txtFilePath = "C:/Path/To/file.txt";
+    String txtFilePath = "C:/Path/To/data.txt";
     int minSupport = 2;
 
-    test(minSupport);
+    //test(minSupport);
     //runFPGrowthFromFile(txtFilePath, minSupport);
-    //decisionTree(minSupport);
+    //decisionTreeCSV();
+    //decisionTreeArff();
   }
 
   public static void test(int minSupport){
@@ -46,11 +53,67 @@ public class TransitDataMining {
     }
   }
 
-  public static void decisionTree(int minSupport){
+  //https://github.com/haifengl/smile/blob/master/shell/src/universal/data/regression/diabetes.csv
+  public static void decisionTreeCSV(){
+    try {
 
+      CSVFormat format = CSVFormat.DEFAULT.withFirstRecordAsHeader();
+      CSV csv = new CSV(format);
+      csv.schema((StructType)null);
 
+      DataFrame data = csv.read(java.nio.file.Paths.get("C:/Path/To/diabetes.csv"));
+      Formula formula = Formula.lhs("y");// predict the "y" column
+
+      DecisionTree model = DecisionTree.fit(formula, data);
+      System.out.println(model.dot());// put the output of this into http://viz-js.com/
+
+      DataFrame dataPredict = csv.read(java.nio.file.Paths.get("C:/Path/To/diabetesPredict.csv"));
+
+      System.out.println("\nPredictions:");
+      System.out.println(Arrays.toString(model.predict(dataPredict)));
+
+      /*
+      int[] prediction = model.predict(dataPredict);
+      for(int i = 0; i < prediction.length; i++) {
+        System.out.println(prediction[i]);
+      }
+      */
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
   }
 
+  //https://github.com/haifengl/smile/blob/8a794175832549ba7057de95bb2bbce032eabce1/shell/src/universal/data/weka/weather.arff
+  public static void decisionTreeArff(){
+    try{
+      Formula formula = Formula.lhs("play");// predict the "play" column
+      Arff arff = new Arff(java.nio.file.Paths.get("C:/Path/To/weather.arff"));
+
+      DataFrame data = arff.read();
+
+      DecisionTree model = DecisionTree.fit(formula, data);
+      System.out.println(model.dot());// put the output of this into http://viz-js.com/
+
+      Arff arffPredict = new Arff(java.nio.file.Paths.get("C:/Path/To/weatherPredict.arff"));
+      DataFrame dataPredict = arffPredict.read();
+
+      System.out.println("\nPredictions:");
+      System.out.println(Arrays.toString(model.predict(dataPredict)));
+
+      /*
+      int[] prediction = model.predict(dataPredict);
+      for(int i = 0; i < prediction.length; i++) {
+        System.out.println(prediction[i]);
+      }
+      */
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(-1);
+    }
+  }
+
+  //used by runFPGrowthFromFile
   //https://github.com/haifengl/smile/blob/2e1c56edb010ea8c6a7eb3f6d9486ae2cf16da87/core/src/test/java/smile/association/ItemSetTestData.java
   public interface ReadFromFile {
 
@@ -75,5 +138,4 @@ public class TransitDataMining {
       return Stream.empty();
     }
   }
-
 }
